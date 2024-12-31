f6fcfe3c45aca7eaac09dd2b83c4e0c1f368c412
CiocanAlexandru <alexciocan2001@gmail.com>
2024-12-31 15:50:02 +0200

    First Commit

---
+++ JavaReview/src/main/java/Lab1/Lab1.java
@@ -0,0 +1,10 @@
+package Lab1;
+
+public class Lab1 {
+
+public static void problemeSolves()
+{
+	System.out.print("This is were you will solve de problemes clean and easy :)) dont give up believe in you");
+}
+	
+}
---
+++ JavaReview/src/main/java/mavensample/JavaReview/App.java
@@ -0,0 +1,9 @@
+package mavensample.JavaReview;
+import Lab1.Lab1;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Lab1.problemeSolves();
+    }
+}
